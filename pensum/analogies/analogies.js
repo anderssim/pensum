3bd611c1e7de5d67d6c1faa38ccb0e19485b2138
anderssim <anders.simonsen@tungstenautomation.com>
2025-05-22 20:47:57 +0200

    Sigh, forgot to add new files

---
+++ pensum/analogies/analogies.js
@@ -0,0 +1,127 @@
+const Analogies = (() => {
+    
+    
+    
+    const il_jang = () =>{
+        return {
+            "gwe": {
+                "name": "Geon",
+                "number": 1,
+                "emoji": "☰",
+                "element": "☁️",
+                "primary": "Himlen",
+                // "secondary": "Begyndelsen",
+            }
+        }
+    }
+
+    const yi_jang = () =>{
+        return {
+            "gwe": {
+                "name": "Tae",
+                "number": 2,
+                "emoji": "☱",
+                "element": "🌊",
+                "primary": "Søen",
+                // "secondary": "Glæden",
+            }
+        }
+    }
+
+    const sam_jang = () => {
+        return {
+            "gwe": {
+                "name": "Ri",
+                "number": 3,
+                "emoji": "☲",
+                "element": "🔥",
+                "primary": "Ild",
+                // "secondary": "Varme",
+            }
+        }
+    }
+
+    const sah_jang = () => {
+        return {
+            "gwe": {
+                "name": "Jin",
+                "number": 4,
+                "emoji": "☵",
+                "element": "🌩️",
+                "primary": "Lyn",
+                // "secondary": "Bevægelse",
+            }
+        }   
+    }
+
+    const oh_jang = () => {
+        return {
+            "gwe": {
+                "name": "Seon",
+                "number": 5,
+                "emoji": "☶",
+                "element": "🌬️ 🌪️",
+                "primary": "Vind",
+                // "secondary": "Bevægelse",
+            }
+        }   
+    }
+
+    const yuk_jang = () => {
+        return {
+            "gwe": {
+                "name": "Gam",
+                "number": 6,
+                "emoji": "☴",
+                "element": "💧",
+                "primary": "Vand",
+                // "secondary": "Bevægelse",
+            }
+        }   
+    }
+
+    const chil_jang = () => {
+        return {
+            "gwe": {
+                "name": "Gan",
+                "number": 7,
+                "emoji": "☷",
+                "element": "⛰️",
+                "primary": "Bjerg",
+            },
+        }
+    }
+
+    const pal_jang = () => {
+        return {
+            "gwe": {
+                "name": "Gon",
+                 "number": 8,
+                "emoji": "☳",
+                "element": "⛰️",
+                "primary": "Jord",
+            },
+        }
+    }
+
+    const koryo = () => {
+        return {
+            "gwe": null,
+        }
+    }
+
+
+    return {
+        il_jang,
+        yi_jang,
+        sam_jang,
+        sah_jang,
+        oh_jang,
+        yuk_jang,
+        chil_jang,
+        pal_jang,
+        koryo
+    }
+    
+
+})();
